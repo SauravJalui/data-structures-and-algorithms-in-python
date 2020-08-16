9b813815331ead1f2c78093597ea880afecf5483
SauravJalui <sauravjalui.007@gmail.com>
2020-08-16 19:42:30 +0530

    Added BinarySearch using Iteration

---
+++ Searching Algorithms/BinarySearchIterative.py
@@ -0,0 +1,18 @@
+def BinarySearch(A, key):
+    low = 0
+    high = len(A) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if key == A[mid]:
+            return True
+        elif key < A[mid]:
+            high = mid - 1
+        elif key > A[mid]:
+            low = mid + 1
+    return False
+
+A = [15, 21, 47, 84, 96]
+found = BinarySearch(A, 96)
+print("The element 96  is found ?: ", found)
+
