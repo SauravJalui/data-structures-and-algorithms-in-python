fdc9192476ebf476aba923e1c07a02d06386ecef
SauravJalui <sauravjalui.007@gmail.com>
2020-08-17 21:15:59 +0530

    Adding PythonBuilt-inSort function

---
+++ Sorting Algorithms/PythonBuiltInSort.py
@@ -0,0 +1,20 @@
+A = [84, 21, 96, 15, 47] 
+print("Original List: ", A)
+A.sort()
+print("Sorted List: ", A)
+
+A = [84, 21, 96, 15, 47] 
+print("Original List: ", A)
+B = sorted(A)
+print("Original List: ", A)
+print("Sorted List: ", B)
+
+A = [84, 21, 96, 15, 47] 
+print("Original List: ", A)
+A.sort(reverse=True)
+print("Sorted List: ", A)
+
+A = ['red', 'blue', 'pink', 'green', 'purple'] 
+print("Original List: ", A)
+A.sort()
+print("Sorted List: ", A)
