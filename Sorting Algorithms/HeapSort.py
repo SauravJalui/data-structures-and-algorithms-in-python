4c211db333999e0df1799be98e8fd43d6a0f88ac
SauravJalui <sauravjalui.007@gmail.com>
2020-08-17 21:08:44 +0530

    Adding QuickSort

---
+++ Sorting Algorithms/HeapSort.py
@@ -0,0 +1,56 @@
+from exceptions import Empty
+
+class ArrayHeap:
+    def __init__(self):
+        self.maxsize = 10
+        self.data = [-1] * self.maxsize
+        self.currentsize = 0
+
+    def __len__(self):
+        return len(self.data)
+    def isEmpty(self):
+        return len(self.data) == 0
+
+    def maxh(self):
+        if self.currentsize == 0:
+            raise Empty("heap is empty")
+        return self.data[1]
+
+    def insert(self, e):
+        if self.currentsize == self.maxsize:
+            raise Empty("no space")
+        self.currentsize += 1
+        i = self.currentsize
+        while i != 1 and e < self.data[i//2]:
+            self.data[i] = self.data[i//2]
+            i = i//2
+        self.data[i] = e
+
+    def deletemin(self):
+        if self.currentsize == 0:
+            raise Empty("Heap is empty")
+        x = self.data[1]
+        y = self.data[self.currentsize]
+        self.currentsize -= 1
+        i = 1
+        ci = 2
+        while ci <= self.currentsize:
+            if ci < self.currentsize and self.data[ci] > self.data[ci + 1]:
+                ci += 1
+            if y <= self.data[ci]:
+                break
+            self.data[i] = self.data[ci]
+            i = ci
+            ci = ci * 2
+        self.data[i] = y
+        return x
+
+h = ArrayHeap()
+h.insert(25)
+h.insert(14)
+h.insert(2)
+h.insert(20)
+h.insert(10)
+h.insert(12)
+for i in range(h.currentsize):
+    print(h.deletemin(), end=' , ')
